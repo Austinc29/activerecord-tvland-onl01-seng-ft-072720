bfecf8e971e3ac1114518e0fbb772babd2c86647
Austin Cummings <aac729@live.com>
2020-09-04 17:58:30 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,7 +1,6 @@
 class Character < ActiveRecord::Base
    belongs_to :actor
    belongs_to :show
-  
     def say_that_thing_you_say 
       self = Character.new(name: "Steve Urkel")
       self.catchphrase = "Did I do that?"
